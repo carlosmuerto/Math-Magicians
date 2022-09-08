fd57fd71975b9adeab2436931f54a7da87c696ac
Carlos Vivas <carlosmuerto@gmail.com>
2022-09-08 13:41:28 -0400

    function component refactoring

--- src/components/Calculator/ClaculatorScreen.js
+++ src/components/Calculator/ClaculatorScreen.js
@@ -1,16 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class CalculatorScreen extends React.Component {
-  render() {
-    const { value } = this.props;
-    return (
-      <div className="calculator-screen">
-        <p className="calculator-value">{value}</p>
-      </div>
-    );
-  }
+function CalculatorScreen({ value }) {
+  return (
+    <div className="calculator-screen">
+      <p className="calculator-value">{value}</p>
+    </div>
+  );
 }
 
 CalculatorScreen.propTypes = {
