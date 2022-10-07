88410ffadeea6345e80926040a33372e0b6e1a47
Carlos Vivas <carlosmuerto@gmail.com>
2022-10-07 18:11:45 -0400

    Add meta info

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Helmet } from 'react-helmet';
 import App from './presentation/App';
 import reportWebVitals from './modules/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <meta
+        property="og:image"
+        content="https://user-images.githubusercontent.com/34493013/194669334-9fcb4d2e-77ba-4c87-a38d-886b5811cb95.png"
+      />
+    </Helmet>
     <App />
   </React.StrictMode>,
 );
